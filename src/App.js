7be8f2b95b114f4f9f64e23d85ed3b3a01a78fb1
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2022-02-27 23:12:32 -0500

    static index page loads into react

--- src/App.js
+++ src/App.js
@@ -1,3 +1,26 @@
-const App = () => <div>Hello Worlll</div>;
+import { useState } from "react";
+
+const App = () => {
+  const [simpleMockData] = useState(false);
+  return (
+    <>
+      <h1
+        class={`mainheading ${
+          simpleMockData ? "mainheading--datareturned" : ""
+        }`}
+      >
+        Will it ever get warmer?
+      </h1>
+      <p class="instructions">
+        Enter your province and city to find out the temperature in seven days.
+      </p>
+      <div class="inputs">
+        <input type="text" placeholder="Province" class="inputs__input" />
+        <input type="text" placeholder="City" class="inputs__input" />
+        <button class="inputs__btn">Search</button>
+      </div>
+    </>
+  );
+};
 
 export default App;
