acb0c4f1e43cb026a5d6840386ac6d2c40d77e50
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2022-03-19 03:28:38 -0400

    its pushing data down but api call is blocked by cors policy

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import LocationsDataProvider from "./LocationsDataProvider";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <LocationsDataProvider>
+      <App />
+    </LocationsDataProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
