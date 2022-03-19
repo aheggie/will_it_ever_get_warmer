90a7c9b0ad9fbc0aefa525ac6184561a6db12301
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2022-03-19 03:15:29 -0400

    locations provider making sense now

--- src/LocationsDataProvider.js
+++ src/LocationsDataProvider.js
@@ -1,17 +1,28 @@
-import { useContext, useEffect } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const EN_LOCATIONS_URL =
   "https://collaboration.cmc.ec.gc.ca/cmc/cmos/public_doc/msc-data/citypage-weather/site_list_en.geojson";
 
-const FR_LOCATIONS_URL =
+//   not using this for now
+const FR_LOCATIONS_URL = //eslint-disable-line no-unused-vars
   "https://collaboration.cmc.ec.gc.ca/cmc/cmos/public_doc/msc-data/citypage-weather/site_list_fr.geojson";
 
-const LocationsDataProvider = () => {
+export const LocationsContext = createContext();
+
+const LocationsDataProvider = ({ children }) => {
   const [locations, setLocations] = useState({});
 
   useEffect(() => {
-    fetch();
+    fetch(EN_LOCATIONS_URL)
+      .then((res) => res.json())
+      .then(setLocations);
   }, [setLocations]);
+
+  return (
+    <LocationsContext.Provider value={locations}>
+      {children}
+    </LocationsContext.Provider>
+  );
 };
 
 export default LocationsDataProvider;
