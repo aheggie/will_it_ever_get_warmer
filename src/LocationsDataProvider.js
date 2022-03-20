8f878bf9b35cfab93e00ba01dccb5095de9f5859
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2022-03-19 22:09:03 -0400

    weather, location, and loadingstate data passed down

--- src/LocationsDataProvider.js
+++ src/LocationsDataProvider.js
@@ -7,22 +7,49 @@ const EN_LOCATIONS_URL = //eslint-disable-line no-unused-vars
 const FR_LOCATIONS_URL = //eslint-disable-line no-unused-vars
   "https://collaboration.cmc.ec.gc.ca/cmc/cmos/public_doc/msc-data/citypage-weather/site_list_fr.geojson";
 
-const MTL_GEOCODE =
-  "https://api.openweathermap.org/geo/1.0/zip?zip=H2T,CA&appid=130f654faf949256e51b5b6577a2855b";
+const API_KEY = "130f654faf949256e51b5b6577a2855b";
+
+const canadianGeocode = (canadianPostalCode, apiKey) =>
+  `https://api.openweathermap.org/geo/1.0/zip?zip=${canadianPostalCode.slice(
+    0,
+    3
+  )},CA&appid=${apiKey}`;
+
+const weatherRequest = (latitude, longitude, apiKey) =>
+  `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly,alerts&appid=${apiKey}&units=metric`;
 
 export const LocationsContext = createContext();
 
 const LocationsDataProvider = ({ children }) => {
-  const [locations, setLocations] = useState({});
-  console.log(MTL_GEOCODE);
+  const [location, setLocation] = useState("");
+  const [weather, setWeather] = useState({});
+  const [loadingState, setLoadingState] = useState("nodata");
+  console.log(canadianGeocode("H2T", API_KEY));
   useEffect(() => {
-    fetch(MTL_GEOCODE)
+    setLoadingState("loading");
+    fetch(canadianGeocode("H2T", API_KEY))
       .then((res) => res.json())
-      .then(setLocations);
-  }, [setLocations]);
+      .then(({ lat, lon, name }) => {
+        setLocation(name);
+        return fetch(weatherRequest(lat, lon, API_KEY));
+      })
+      .then((res) => res.json())
+      .then((weatherData) => {
+        setWeather({
+          // might want temp max from daily[0]
+          // weatherData.current.temp gives immediate current temp
+          // daily[0].temp.max gives current day's max temp
+          currentHigh: weatherData.daily[0].temp.max,
+          futureHigh:
+            // the last index of weatherData.daily is the furthest day,i.e. seventh
+            weatherData.daily[weatherData.daily.length - 1].temp.max,
+        });
+        setLoadingState("dataloaded");
+      });
+  }, [setLocation]);
 
   return (
-    <LocationsContext.Provider value={locations}>
+    <LocationsContext.Provider value={{ location, weather, loadingState }}>
       {children}
     </LocationsContext.Provider>
   );
