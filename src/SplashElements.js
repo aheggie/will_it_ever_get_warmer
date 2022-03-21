1209d08c3242d8fe78d839814379cc3df7649b61
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2022-03-20 23:24:18 -0400

    postal code typable and validated on length

--- src/SplashElements.js
+++ src/SplashElements.js
@@ -1,21 +1,42 @@
+import { useState } from "react";
+
+const validatePostalCode = (postCode) => postCode.length >= 3;
+
 // this component needs a better name
-const SplashElements = ({ toggleMockDataReturned }) => (
-  <>
-    <p className="instructions">
-      Enter your province and city to find out the temperature in seven days.
-    </p>
-    <div className="inputs">
-      <input type="text" placeholder="Province" className="inputs__input" />
-      <input type="text" placeholder="City" className="inputs__input" />
-      <button
-        className="inputs__btn"
-        onClick={toggleMockDataReturned}
-        onBlur={toggleMockDataReturned}
-      >
-        Search
-      </button>
-    </div>
-  </>
-);
+const SplashElements = ({ toggleMockDataReturned }) => {
+  const [postalCode, setPostalCode] = useState("");
+  return (
+    <>
+      <p className="instructions">
+        Enter your POSTAL CODE to find out the temperature in seven days.
+      </p>
+      <div className="inputs">
+        {/* <input type="text" placeholder="Province" className="inputs__input" />
+      <input type="text" placeholder="City" className="inputs__input" /> */}
+        <input
+          // how to autofocus :
+          // https://stackoverflow.com/questions/28889826/set-focus-on-input-after-render
+          // autofocus is maybe acceptable for accessibility when there is a single clear element :
+          // https://ux.stackexchange.com/questions/60026/when-is-it-appropriate-to-add-the-html5-autofocus-attribute-to-a-page
+          // eslint-disable-next-line jsx-a11y/no-autofocus
+          autoFocus
+          type="text"
+          placeholder="POSTAL CODE"
+          value={postalCode}
+          onChange={(e) => setPostalCode(e.target.value)}
+          className="inputs__input"
+        />
+        <button
+          disabled={!validatePostalCode(postalCode)}
+          className="inputs__btn"
+          onClick={toggleMockDataReturned}
+          onBlur={toggleMockDataReturned}
+        >
+          Search
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default SplashElements;
