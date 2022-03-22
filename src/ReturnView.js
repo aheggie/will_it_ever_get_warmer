958a2fc5670b214c30f6fdbc1b8013fc25071613
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2022-03-22 00:58:46 -0400

    date working

--- src/ReturnView.js
+++ src/ReturnView.js
@@ -2,6 +2,27 @@ import { useContext } from "react";
 import Sentences from "./Sentences";
 import { WeatherAPIContext } from "./WeatherAPIProvider";
 
+const dateDisplayString = (dateObj) => {
+  // const weekday = ["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][dateObj.getDay()]
+  const month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ][dateObj.getMonth()];
+  const monthDay = dateObj.getDate();
+  const year = dateObj.getFullYear();
+  return `${month} ${monthDay}, ${year}`;
+};
+
 const ReturnView = () => {
   const { weather, location } = useContext(WeatherAPIContext);
   const { currentHigh, futureHigh } = weather;
@@ -9,6 +30,9 @@ const ReturnView = () => {
   const tempDiff = futureHigh - currentHigh;
 
   const suffix = tempDiff > 3 ? "warmer" : tempDiff > -2 ? "cold" : "colder";
+
+  const today = new Date();
+  const sevenDays = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
 
   document.getElementById("root").classList.add(`root--${suffix}`);
   return (
@@ -18,12 +42,12 @@ const ReturnView = () => {
       <div className="weatherboxes">
         <div className="weatherboxes__box">
           <p className="weatherboxes__location">{location}</p>
-          <p className="weatherboxes__date">February 26, 2022</p>
+          <p className="weatherboxes__date">{dateDisplayString(today)}</p>
           <p className="weatherboxes__temperature">{currentHigh}</p>
         </div>
         <div className="weatherboxes__box">
           <p className="weatherboxes__location">{location}</p>
-          <p className="weatherboxes__date">March 5, 2022</p>
+          <p className="weatherboxes__date">{dateDisplayString(sevenDays)}</p>
           <p className="weatherboxes__temperature">{futureHigh}</p>
         </div>
       </div>
